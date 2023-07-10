1b85311544d2f0e7743aa70beade1c38a3ab5ea1
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 13:02:54 -0500

    Update archivos.c

--- archivos.c
+++ archivos.c
@@ -1,41 +1,109 @@
 #include<stdio.h>
 #include<stdlib.h>
-int cargarMedicamentos(struct empleado empleados[100]){
-    int n_emp = 0;
-    FILE *meds;
-    f = fopen("medicamentos.txt","r");
+#include<string.h>
+#include "funciones.h"
+int cargarMedicamentos(struct medicamento meds[100]){
+    int numMeds=0;
+    FILE *inv;
+    inv=fopen("Medicamentos.txt", "r");
     char cadena[255];
-    char delimitador[] = ";";
+    char separador[]=";";
 
-    while (feof(f) == 0){
+    while(feof(inv)==0){
+        struct medicamento m;
+        fgets(cadena, 255, inv);
+        char *token = strtok(cadena, separador);
 
-        struct empleado e;
-        fgets(cadena,255,f);
-        char *token = strtok(cadena,delimitador);
+        if (token!=NULL){
+            int espacio=1;
+            while(token!=NULL){
+                if(espacio==1){
+                    strcpy(m.nombre, token);
 
-        if(token != NULL){
-            int campo = 1;
-            while (token != NULL){
-                if (campo == 1){
-                    strcpy(e.nombre,token);
                 }
-                else if(campo == 2){
-                    e.id = atoi(token);
+                else if(espacio==2){
+                    m.codigo=atoi(token);
+
                 }
-                else if(campo == 3){
-                    e.sueldo = atof(token);
+                else if(espacio==3){
+                    m.lote=atoi(token);
+
                 }
-                else if(campo == 4){
-                    e.horas = atoi(token);
+                else if(espacio==4){
+                    m.unidades=atoi(token);
+
                 }
-                campo++;
-                token = strtok(NULL, delimitador);
+                else if(espacio==5){
+                    m.unidadesxCaja=atoi(token);
+
+                }
+                espacio++;
+                token=strtok(NULL, separador);
+
             }
         }
-        empleados[n_emp] = e;
-        n_emp++;
+        meds[numMeds]=m;
+        numMeds++;
 
     }
+    fclose(inv);
+    return(numMeds);
+}
+int cargarPersonas(struct personas pers[100]){
+    int numPer=0;
+    FILE *per;
+    per=fopen("Personas.txt", "r");
+    char cadena[255];
+    char separador[]=";";
+
+    while(feof(per)==0){
+        struct personas p;
+        fgets(cadena, 255, per);
+        char *token = strtok(cadena, separador);
+
+        if (token!=NULL){
+            int espacio=1;
+            while(token!=NULL){
+                if(espacio==1){
+                    strcpy(p.nombre, token);
+
+                }
+                else if(espacio==2){
+                    p.cedula=atoi(token);
+
+                }
+
+                espacio++;
+                token=strtok(NULL, separador);
+
+            }
+        }
+        pers[numPer]=p;
+        numPer++;
+
+    }
+    fclose(per);
+    return(numPer);
+}
+void guardarMedicamentos(struct medicamento meds[100], int numMed){
+    FILE *f;
+    f = fopen("medicamentos.txt","w+");
+
+    for (int i=0; i<numMed-1; i++){
+        fprintf(f, "%s;%d;%d;%d;%d\n",meds[i].nombre,meds[i].codigo,meds[i].lote,meds[i].unidades, meds[i].unidadesxCaja);
+    }
+    fprintf(f, "%s;%d;%d;%d;%d\n",meds[numMed-1].nombre,meds[numMed-1].codigo,meds[numMed-1].lote,meds[numMed-1].unidades, meds[numMed-1].unidadesxCaja);
+
     fclose(f);
-    return n_emp;
+}
+void guardarPersonas(struct personas per[100], int numPer){
+    FILE *f;
+    f = fopen("Personas.txt","w+");
+
+    for (int i=0; i<numPer-1; i++){
+        fprintf(f, "%s;%d\n",per[i].nombre,per[i].cedula);
+    }
+    fprintf(f, "%s;%d\n",per[numPer-1].nombre,per[numPer-1].cedula);
+
+    fclose(f);
 }
