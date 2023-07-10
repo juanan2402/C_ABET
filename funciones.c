d274c8e602b7e91cee692acb7ffedc39861ded2f
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 08:58:48 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -61,3 +61,19 @@ void vectorMeds(char meds[][100], int numMed){
     }  
     
 }
+int Menu (int opcion) { 
+    do { 
+        printf("\n*********************************\n"); 
+        printf("* Menu principal *\n"); 
+        printf("* ---------------- *\n"); 
+        printf("* 1. Entrada de producto *\n"); 
+        printf("* 2. Despacho de producto *\n"); 
+        printf("* 3. Reporte del inventario *\n"); 
+        printf("* 4. Salir *\n")
+        printf("*********************************\n\n"); 
+        scanf ("%d",& opcion); 
+        if (opcion<1 || opcion>4){ 
+            printf("Opcion invalida por favor elija las opciones presentadas"); 
+        } 
+    } while (opcion!=4); 
+} 
