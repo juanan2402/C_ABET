d7938d8b6a3744b78eb477d627eda64443785180
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 13:01:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,65 +1,44 @@
-#include<stdio.h>
-#include "Menu.h"
-#include "Entrada.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "archivos.h"
 #include "funciones.h"
+int main()
+{
+    int opcion=0;
+    int numMed=0, numPer=0;
+    struct medicamento meds[100];
+    struct personas pers[100];
+    numMed=cargarMedicamentos(meds);
+    numPer=cargarPersonas(pers);
 
-int main(){ 
-    struct med medicinas[100]
-    char clientes[100][100];
-    char meds[100][100];
-    //File Creation
-    FILE *clientes;
-    FILE *mediamentos;
-    FILE *entradas;
-    FILE *salidas;
+    do{
+        Menu();
+        scanf("%d", &opcion);
+        switch(opcion){
+        case 1:
+            printf("Orden de ingreso\n");
+            int creado=IngresoProductos(meds, numMed);
+            if(creado==1){
+                numMed++;
+            }
+            break;
+        case 2:
+            printf("Orden de despacho\n");
+            despacho(meds, numMed, pers, numPer);
+            break;
+        case 3:
+            printf("INVENTARIO: \n");
+            Inventario(meds, numMed);
+            break;
 
-    clientes = fopen("clientes.txt", "w+");
-    if (clientes == NULL)
-    {
-        printf("No se puede crear el archivo\n");
-    }else
-    {
-        printf("Se ha creado el archivo clientes.txt\n");
-        fclose(clientes);
-    } 
-
-    entradas = fopen("entradas.txt", "w+");
-    if (entradas == NULL)
-    {
-        printf("No se puede crear el archivo\n");
-    }else
-    {
-        printf("Se ha creado el archivo salidas.txt\n");
-        fclose(entradas);
-    } 
-
-    salidas = fopen("salidas.txt", "w+");
-    if (salidas == NULL)
-    {
-        printf("No se puede crear el archivo\n");
-    }else
-    {
-        printf("Se ha creado el archivo salidas.txt\n");
-        fclose(salidas);
-    } 
-
-    medicamentos = fopen("medicamentos.txt", "w+");
-        if (medicamentos == NULL)
-        {
-            printf("No se puede crear el archivo\n");
-        }else
-        {
-            printf("Se ha creado el archivo medicamentos.txt\n");
-            fclose(medicamentos);
-        } 
-
-    int opcion, entrada; 
-    switch (Menu (opcion)){ 
-        case 1: Entrada_de_producto (entrada); 
-        break; 
-        case 2: 
-        break; 
-        case 3: 
-        break; 
-    } 
+        case 4:
+            printf("Listado Personas: \n");
+            Personas(pers, numPer);
+            break;
+        }
+    }while(opcion!=5);
+    guardarMedicamentos(meds, numMed);
+    guardarPersonas(pers, numPer);
+    printf("GRACIAS POR SU TRANSACCION, QUE LE VAYA MUY BIEN!\n\n");
+    return 0;
 }
