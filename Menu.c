5b9905e59558c3b96afe2144453fec2ff0b36dcb
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 08:54:07 -0500

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -8,11 +8,12 @@ int Menu (int opcion) {
         printf("* 1. Entrada de producto *\n"); 
         printf("* 2. Despacho de producto *\n"); 
         printf("* 3. Reporte del inventario *\n"); 
+        printf("* 4. Salir *\n")
         printf("*********************************\n\n"); 
         scanf ("%d",& opcion); 
-        if (opcion<1 || opcion>3){ 
+        if (opcion<1 || opcion>4){ 
             printf("Opcion invalida por favor elija las opciones presentadas"); 
         } 
-    } while (opcion<1 || opcion>3); 
+    } while (opcion!=4); 
 } 
 
