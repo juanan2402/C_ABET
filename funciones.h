534bebed1d5c39634b17e0235c858c5a3876f58d
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 09:04:41 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,5 +1,12 @@
 #include "funciones.c"
+struct med{
+  char nombre[100];
+  int codigo;
+  int lote;
+  int cantidadIndividual;
+  int cantidadPorCajas;
 
+}
 void ticket_de_recibo();
 void creacion_de_producto();
 void creacion_de_cliente();
