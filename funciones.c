4ed9f1ae0afc158e5112f592def0a279a7e07411
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-07-10 13:01:50 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,79 +1,195 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
-void ticket_de_recibo(){
+
+struct medicamento{
+    char nombre[100];
+    int codigo;
+    int lote;
+    int unidades;
+    int unidadesxCaja;
+};
+struct personas{
+
+    char nombre[100];
+    int cedula;
+};
+int comprobarCodigo(struct medicamento med[100], int numMed, int codigo);
+int comprobarCedula(struct personas per[100], int numPer, int cedula);
+void modificarCantidades(struct medicamento med[100], int numMed, int codigo);
+void Menu () {
+        printf("\n*********************************\n");
+        printf("* Menu principal *\n");
+        printf("* ---------------- *\n");
+        printf("* 1. Entrada de producto *\n");
+        printf("* 2. Despacho de producto *\n");
+        printf("* 3. Reporte del inventario *\n");
+        printf("* 4. Listado de personas *\n");
+        printf("* 5. Salir *\n");
+        printf("*********************************\n\n");
 
 }
-int existe_producto(){
-    
-}
-void creacion_de_producto(char productos []){
-    FILE *medicamentos;
-    medicamentos = fopen("medicamentos.txt", "r+");
-    int numMed, lote, cantidadIndividual, cantidadCajas, pos;
-    char nombre[50];
-    do {
-    printf("Ingrese el nombre del medicamento:\n");
-    scanf("%s",nombre);
-    if(existe_producto()!=0){
+void Inventario(struct medicamento meds[100], int numMeds){
+    for (int i=0; i<numMeds; i++){
+        printf("Nombre: %s\n", meds[i].nombre);
+        printf("codigo del medicamento: %d\n", meds[i].codigo);
+        printf("lote: %d\n", meds[i].lote);
+        printf("Unidades disponibles: %d\n",meds[i].unidades);
+        printf("cajas disponibles: %d\n",meds[i].unidades/meds[i].unidadesxCaja);
+        printf("**********************************\n");
 
     }
-    printf("Ingrese el lote:\n");
-    scanf("%d",&lote);
-    printf("Ingrese la cantidad individual recibida:\n");
-    scanf("%d",&cantidadIndividual);
-    printf("Ingrese la cantidad de medicamentos por caja:\n");
-    scanf("%d",&cantidadCajas);
-        numMed++;
-        fprintf(medicamentos, "%d %s \n", numMed nombre);
+
+}
+void Personas(struct personas per[100], int numPer){
+    for (int i=0; i<numPer; i++){
+        printf("Nombre: %s\n", per[i].nombre);
+        printf("cedula: %d\n", per[i].cedula);
+        printf("**********************************\n");
     }
-    printf("el torneo sera de %d equipos\n", numequipo);
-    fclose(equipo);
-    return numequipo;
-
 
 }
 
-void creacion_de_cliente(){
+int IngresoProductos(struct medicamento med[100], int numMed){
 
-}
-void transaccion_entrada(){
+    int creado=0;
+    struct medicamento m;
+    fflush(stdin);
+    printf("introducir codigo del medicamento:\n");
+    scanf("%d", &m.codigo);
+    int existe=comprobarCodigo(med, numMed, m.codigo);
+    if (numMed<100){
+        if(existe==0){
+            printf("introducir nombre del medicamento:\n");
+            scanf("%s", m.nombre);
+            printf("introducir lote del medicamento:\n");
+            scanf("%d", &m.lote);
+            printf("introducir unidades ingresadas del medicamento:\n");
+            scanf("%d", &m.unidades);
+            printf("Especificar cuantas unidades hay por caja\n");
+            scanf("%d", &m.unidadesxCaja);
+            med[numMed]=m;
+            creado=1;
+            }
+        else{
+            int eliminado=0;
+            int codigo, indice;
+            codigo=m.codigo;
+            int existe = comprobarCodigo(med,numMed,codigo);
+            if (existe == 1){
+                int entrada;
+                printf("Introduce la cantidad en unidades que se aumentan\n");
+                scanf("%d",&entrada);
+                for(int i=0; i<numMed; i++){
+                    if (med[i].codigo == codigo){
+                        med[i].unidades = med[i].unidades+entrada;
+                        printf("Cantidades actualizadas\n");
+                    }
+                }
 
-}
-void transaccion_salida(){
-
-}
-
-void vectorMeds(char meds[][100], int numMed){
-    FILE *medicamentos;
-    medicamentos= fopen("medicamentos.txt", "r+");
-    
-    if (medicamentos == NULL)
-    {
-        printf("No se puede leer el archivo\n");
-    }else
-    {
-        for(int i=0; i<numMed; i++){
-            fscanf(medicamentos, "%s",meds[i]);
+            }
+            else{
+                printf("No se puede modificar el medicamento, no existe el codigo\n");
+            }
         }
-        fclose(medicamentos);
-    }  
-    
+
+    }
+    else{
+        printf("limite de medicamentos en bodega\n");
+    }
+
+
+    return creado;
 }
-int Menu (int opcion) { 
-    do { 
-        printf("\n*********************************\n"); 
-        printf("* Menu principal *\n"); 
-        printf("* ---------------- *\n"); 
-        printf("* 1. Entrada de producto *\n"); 
-        printf("* 2. Despacho de producto *\n"); 
-        printf("* 3. Reporte del inventario *\n"); 
-        printf("* 4. Salir *\n")
-        printf("*********************************\n\n"); 
-        scanf ("%d",& opcion); 
-        if (opcion<1 || opcion>4){ 
-            printf("Opcion invalida por favor elija las opciones presentadas"); 
-        } 
-    } while (opcion!=4); 
-} 
+
+
+int IngresoPersonas(struct personas per[100], int numPer){
+
+    int creado=0;
+    struct personas p;
+    fflush(stdin);
+    printf("introducir nombre del cliente:\n");
+    scanf("%s", p.nombre);
+    printf("introducir la cedula del cliente:\n");
+    scanf("%d", &p.cedula);
+    int existe=comprobarCedula(per, numPer, p.cedula);
+    if (numPer<100){
+        if(existe==0){
+            per[numPer]=p;
+            creado=1;
+            }
+        else{
+            printf("Cedula duplicada\n");
+        }
+
+    }
+    else{
+        printf("limite de personas alcanzado\n");
+    }
+    return creado;
+}
+
+void despacho(struct medicamento med[100], int numMed, struct personas per[100], int numPer){
+    int eliminado=0;
+    int codigo, indice, cedula;
+    printf("Ingresar el numero de cedula que desea comprar\n");
+    scanf("%d",&cedula);
+        int existePer = comprobarCedula(per,numPer,cedula);
+        if (existePer == 1){
+                do{
+                    printf("Introduce el codigo del medicamento\n");
+                    scanf("%d",&codigo);
+                    int existe = comprobarCodigo(med,numMed,codigo);
+                    if (existe == 1){
+                        int salida;
+                        printf("Introduce la cantidad en unidades que deseas quitar\n");
+                        scanf("%d",&salida);
+                        for(int i=0; i<numMed; i++){
+                            if (med[i].codigo == codigo){
+                                med[i].unidades = med[i].unidades-salida;
+                                printf("Cantidades actualizadas\n");
+                            }
+                        }
+
+                    }
+                    else{
+                        printf("No se puede modificar el medicamento, no existe el codigo\n");
+                    }
+        }while(codigo!=0);
+        }
+        else{
+            IngresoPersonas(per, numPer);
+            printf("Usuario registrado, ingrese nuevamente para registrar su entrega\n");
+            guardarPersonas(per, numPer);
+        }
+
+
+}
+
+
+int comprobarCodigo(struct medicamento med[100], int numMed, int codigo){
+
+    int resultado = 0;
+
+    for (int i=0; i<numMed; i++){
+        if (med[i].codigo== codigo){
+            resultado = 1;
+        }
+    }
+
+    return resultado;
+
+}
+int comprobarCedula(struct personas per[100], int numPer, int cedula){
+
+    int resultado = 0;
+
+    for (int i=0; i<numPer; i++){
+        if (per[i].cedula== cedula){
+            resultado = 1;
+        }
+    }
+
+    return resultado;
+
+}
